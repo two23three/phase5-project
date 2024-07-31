a99fb880bcb65e3ea73f4e6b8eef90928c44a0ad
matthewmwachi <mattmuhande@gmail.com>
2024-07-31 12:29:44 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ChangeAccountDetails from './components/ChangeAccountDetails'
+import ChangePassword from './components/ChangePassword'
+import EditTransaction from './components/EditTransaction'
+import LogOutPopUp from './components/LogOutPopUp'
+import MpesaPopUp from './components/MpesaPopUp'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,10 +14,18 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/changedetails" element={<ChangeAccountDetails />} />
+          <Route path="/changepassword" element={<ChangePassword />} />
+          <Route path="/edittransaction" element={<EditTransaction/>} />
+          <Route path="/logout" element={<LogOutPopUp />} />
+          <Route path="/mpesa" element={<MpesaPopUp />} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
-            
+
 export default App
