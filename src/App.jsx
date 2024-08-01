5ed56ada83f39d2fa19427fecea01f041e521191
Bjoseph23 <typicalomondi@gmail.com>
2024-08-01 13:36:51 +0300

    Added plain register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import ChangeAccountDetails from './components/ChangeAccountDetails'
-import ChangePassword from './components/ChangePassword'
-import EditTransaction from './components/EditTransaction'
-import LogOutPopUp from './components/LogOutPopUp'
-import MpesaPopUp from './components/MpesaPopUp'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Register from './pages/Register';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
       <div>
-        <Switch>
-          <Route path="/changedetails" element={<ChangeAccountDetails />} />
-          <Route path="/changepassword" element={<ChangePassword />} />
-          <Route path="/edittransaction" element={<EditTransaction/>} />
-          <Route path="/logout" element={<LogOutPopUp />} />
-          <Route path="/mpesa" element={<MpesaPopUp />} />
-        </Switch>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
