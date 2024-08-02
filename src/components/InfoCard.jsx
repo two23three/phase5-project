1defe50ac44fdfce82738015f668e77953ea413a
iankuria668 <iankuria668@gmail.com>
2024-08-02 17:04:53 +0300

    InfoCard spelling error corrected

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function InforCard({title, value}){
+function InfoCard({title, value}){
 return(
     <div className="bg-white rounded-lg shadow-lg">
         <h2>{title}</h2> <h2>{value}</h2>
     </div>
 )
 }
-export default InforCard
+export default InfoCard
