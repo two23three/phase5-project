f1086ba07bee0e5170bcf6adc3119b934b4928b8
iankuria668 <iankuria668@gmail.com>
2024-08-02 10:29:07 +0300

    Home page created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useEffect } from "react";
+
+function Home() {
+    useEffect
+return (
+    <div>
+    <p>Home</p>
+    <div>
+        <h2>Income</h2> <h2>Ksh 300,000</h2>
+    </div>
+    <div>
+        <h2>Expenses</h2> <h2>Ksh 300,000</h2>
+    </div>
+    <div>
+        <h2>Assets</h2> <h2>Ksh 300,000</h2>
+    </div>
+    <div>
+        <h2></h2>
+    </div>
+    </div>
+)
+
+};
+export default Home;
