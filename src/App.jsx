39407526dbe9fde1b9ffe889bcfab22f1348a635
MwachiOfficial <162096070+MwachiOfficial@users.noreply.github.com>
2024-08-02 11:33:33 +0300

    Update routing

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,14 @@ function App() {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/add_transaction" element={<AddTransaction />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/assets" element={<Assets />} />
+          <Route path="/budget" element={<Budget />} />
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/insights" element={<Insights />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/more" element={<More />} />
         </Routes>
       </div>
     </Router>
