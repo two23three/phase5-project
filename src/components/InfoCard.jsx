d5a961a8df916cfb4acfdcda1586b2a69f8b463e
iankuria668 <iankuria668@gmail.com>
2024-08-02 10:47:12 +0300

    Info card corrected

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -3,16 +3,9 @@ import {Card, CardContent, Typography} from "@mui/material";
 
 function InforCard({title, value}){
 return(
-    <Card>
-        <CardContent>
-            <Typography variant="h5" component="div">
-                {title}
-            </Typography>
-            <Typography variant="body2">
-                {value}
-            </Typography>
-        </CardContent>
-    </Card>
+    <div className="bg-white rounded-lg shadow-lg">
+        <h2>{title}</h2> <h2>{value}</h2>
+    </div>
 )
 }
 export default InforCard
