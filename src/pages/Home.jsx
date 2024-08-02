ddc4ac4bc2b1a7b898fa49b62bfb4a4b27a7593e
iankuria668 <iankuria668@gmail.com>
2024-08-02 11:02:27 +0300

    Home fetch created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,52 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import PieChart from "../components/PieChart";
+import InforCard from "../components/InfoCard";
 
 function Home() {
-    useEffect
-return (
-    <div>
-    <p>Home</p>
-    <div>
-        <h2>Income</h2> <h2>Ksh 300,000</h2>
-    </div>
-    <div>
-        <h2>Expenses</h2> <h2>Ksh 300,000</h2>
-    </div>
-    <div>
-        <h2>Assets</h2> <h2>Ksh 300,000</h2>
-    </div>
-    <div>
-        <h2></h2>
-    </div>
-    </div>
-)
+    const [data, setData] = useState(({
+        income: 0,
+        expense: 0,
+        assets: 0,
+        loans: 0,
+        savings: 0,
+    }));
 
+    const API_URL = "https://bizzgogo-70f9.onrender.com/admin"
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch(API_URL);
+                const data = await response.json();
+                setData({
+                    income: data.income,
+                    expense: data.expense,
+                    assets: data.assets,
+                    loans: data.loans,
+                    savings: data.savings
+                });
+            } catch (error) {
+                console.log('Error fetching data:', error);
+            }
+            };
+        fetchData();
+    }, []);
+    let balance = data.income - data.expense;
+    return(
+        <div>
+            <div>
+            <PieChart data = {data} />
+            <h2>{balance}</h2>
+            <p>left to spend</p>
+            </div>
+            <div>
+                <InforCard title="Income" value={data.income} />
+                <InforCard title="Expense" value={data.expense} />
+                <InforCard title="Assets" value={data.assets} />
+                <InforCard title="Loans" value={data.loans} />
+                <InforCard title="Savings" value={data.savings} />
+            </div>
+        </div>
+
+    )
 };
-export default Home;
