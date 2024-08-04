68570336cd189c5d8aec0dd7fd88b1593d97ce9f
Muny1re1 <munyiriandy@gmail.com>
2024-08-04 19:18:04 +0300

    insights

--- src/pages/Insights.jsx
+++ src/pages/Insights.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import {Chart as Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler} from 'chart.js';
+import './Insights.css';
 
 
 
