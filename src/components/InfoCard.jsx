dab352fe74e6bc5d9a832aa045aa4e01b369eae3
Bjoseph23 <typicalomondi@gmail.com>
2024-08-05 11:52:56 +0300

    Fixed tailwind config file

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-=======
 // import {Card, CardContent, Typography} from "@mui/material";
->>>>>>> 88aceec (Added register page)
 
 function InfoCard({title, value}){
 return(
