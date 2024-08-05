019dc6e95219d9cb03b21025970f5e68c623e1d7
iankuria668 <iankuria668@gmail.com>
2024-08-05 23:14:50 +0300

    Navbar slightly formatted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faChartBar, faPlus, faWallet, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
     return (
-        <nav id='navbar' className="flex space-x-4">
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/insights'>Insights</NavLink>
+        <nav id='navbar' className="flex space-x-4 bg-gray-800">
+            <NavLink to='/' className="flex flex-col items-center text-white">
+                <FontAwesomeIcon icon={faHome} size="2x" />
+                <span>Home</span>
+            </NavLink>
+            <NavLink to='/insights' className="flex flex-col items-center text-white">
+                <FontAwesomeIcon icon={faChartBar} size="2x" />
+                <span>Insights</span>
+            </NavLink>
             <NavLink to='/add_transaction'>
                 <button className="bg-red-500 text-white w-10 h-10 flex items-center justify-center rounded">
                     <FontAwesomeIcon icon={faPlus} />
                 </button>
             </NavLink>
-            <NavLink to='/budget'>Budget</NavLink>
-            <NavLink to='/p-more'>More</NavLink>
+            <NavLink to='/budget' className="flex flex-col items-center text-white">
+                <FontAwesomeIcon icon={faWallet} size="2x" />
+                <span>Budget</span>
+            </NavLink>
+            <NavLink to='/p-more' className="flex flex-col items-center text-white">
+                <FontAwesomeIcon icon={faEllipsisH} size="2x" />
+                <span>More</span>
+            </NavLink>
         </nav>
     );
 }
