e328d992327cc71f283e14f675bbd97870b28223
iankuria668 <iankuria668@gmail.com>
2024-08-05 23:09:03 +0300

    Icons intergrted into the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
     return (
-        <nav id='navbar'>
+        <nav id='navbar' className="flex space-x-4">
             <NavLink to='/'>Home</NavLink>
             <NavLink to='/insights'>Insights</NavLink>
-            <NavLink to='/add_transaction'> Add Transaction</NavLink>
+            <NavLink to='/add_transaction'>
+                <button className="bg-red-500 text-white w-10 h-10 flex items-center justify-center rounded">
+                    <FontAwesomeIcon icon={faPlus} />
+                </button>
+            </NavLink>
             <NavLink to='/budget'>Budget</NavLink>
             <NavLink to='/p-more'>More</NavLink>
         </nav>
