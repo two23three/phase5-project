24c81e7fa50b0f8db7f12312b9181d619003cfa7
Muny1re1 <munyiriandy@gmail.com>
2024-08-05 14:02:31 +0300

    added chart, date filter and icons to the income page

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Chart, Line } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from 'chart.js';
 
 ChartJS.register(
