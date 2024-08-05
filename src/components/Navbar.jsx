bf8e55ad8420b861a3b018f898a9eff1b11aa46b
iankuria668 <iankuria668@gmail.com>
2024-08-05 22:58:50 +0300

    Typpo corrected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function Navbar(){
-    return(
+function Navbar() {
+    return (
         <nav id='navbar'>
             <NavLink to='/'>Home</NavLink>
-            <Navlink to='/insights'>Insights</Navlink>
-            <button to='/add_transaction'>Add Transaction</button>
+            <NavLink to='/insights'>Insights</NavLink>
+            <NavLink to='/add_transaction'> Add Transaction</NavLink>
             <NavLink to='/budget'>Budget</NavLink>
             <NavLink to='/p-more'>More</NavLink>
-      </nav>
-    )
+        </nav>
+    );
 }
 
-export  default Navbar
+export default Navbar;
