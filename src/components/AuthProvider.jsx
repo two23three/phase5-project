3a0b0ed1a42c4d962e0910482efd492bf9637f82
Bjoseph23 <typicalomondi@gmail.com>
2024-08-12 05:29:42 +0300

    Added state in the AuthProvider component

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -6,19 +6,24 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token') || '');
+  const [user, setUser] = useState(null); 
 
-  const login = (newToken) => {
-    setToken(newToken);
+  const login = (newToken, userData) => {
+    setToken(newToken);// Save the taoken for the user logged in
+    setUser(userData); // Save user data for the user logged in
     localStorage.setItem('token', newToken);
+    localStorage.setItem('user', JSON.stringify(userData));
   };
 
   const logout = () => {
     setToken('');
+    setUser(null); // Clear user data
     localStorage.removeItem('token');
+    localStorage.removeItem('user');
   };
 
   return (
-    <AuthContext.Provider value={{ token, login, logout }}>
+    <AuthContext.Provider value={{ token, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
-
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins:[tailwindcss()]
-    }
-  }
+      plugins: [tailwindcss()],
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://barnes.onrender.com', 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
+    },
+  },
 })
