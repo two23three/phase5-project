767f5e5ae18b15a0b0866938108e0b398e1e90d1
iankuria668 <iankuria668@gmail.com>
2024-08-12 11:17:29 +0300

    Loading screen intergrated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ function Home() {
     const [debt, setDebt] = useState(0);
     const [assets, setAssets] = useState(0);
     const [currency, setCurrency] = useState("Ksh");
+    const [isLoading, setIsLoading] = useState(true);
 
     const {getUserId} = useAuth();
     const userID = getUserId();
@@ -45,8 +46,10 @@ function Home() {
                 });
 
                 setIncome(totalIncome);
+                
             } catch (error) {
                 console.log("Error fetching income:", error);
+                
             }
         };
         fetchData();
@@ -69,8 +72,10 @@ function Home() {
                 });
 
                 setExpense(totalExpenses);
+                
             } catch (error) {
                 console.log("Error fetching expenses:", error);
+            
             }
         };
         fetchData();
@@ -117,8 +122,10 @@ function Home() {
                 });
 
                 setAssets(totalAssets);
+                setIsLoading(false);
             } catch (error) {
                 console.log("Error fetching assets:", error);
+                setIsLoading(false);
             }
         };
         fetchData();
@@ -138,7 +145,19 @@ function Home() {
     }else{
         tempDebt = 0;
     }
-
+    if(isLoading){
+        return (
+            <div className="flex items-center justify-center h-screen">
+              <div className="text-center">
+                <svg className="animate-spin h-8 w-8 text-blue-600 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                <p className="mt-2 text-gray-600">Loading user data...</p>
+              </div>
+            </div>
+          );
+        }
     return (
         <div className="rounded-b-2xl flex flex-col gap-4 bg-gray-900 p-1 ">
             <Header onCurrencyChange={handleCurrencyChange} onLogout={() => console.log("Logged out")} />
