78f8a4f739d0cfbebb4f36586abb9415505829f0
iankuria668 <iankuria668@gmail.com>
2024-08-13 11:19:05 +0300

    responsiveness updated

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function InfoCard({ title, value }) {
     return (
-        <div className="bg-white rounded-lg flex justify-between items-center p-4 text-black mb-4">
+        <div className="bg-white rounded-lg flex justify-between items-center p-4 text-black mb-4 margin-left-5%">
             <h2 className="text-xl font-bold">{title}</h2>
             <h2 className="text-xl">{value}</h2>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,6 @@ import Navbar from "../components/Navbar";
 import { formatNumber } from "chart.js/helpers";
 import { useAuth } from "../components/AuthProvider";
 
-
 function Home() {
     const [income, setIncome] = useState(0);
     const [expense, setExpense] = useState(0);
@@ -16,17 +15,15 @@ function Home() {
     const [currency, setCurrency] = useState("Ksh");
     const [isLoading, setIsLoading] = useState(true);
 
-    const {getUserId} = useAuth();
+    const { getUserId } = useAuth();
     const userID = getUserId();
     const API_URL = "https://barnes.onrender.com/";
-
-    
 
     const currencySymbols = {
         USD: "$",
         EUR: "€",
         GBP: "£",
-        KES: "Ksh"
+        KES: "Ksh",
     };
 
     useEffect(() => {
@@ -34,22 +31,19 @@ function Home() {
             try {
                 const response = await fetch(`${API_URL}incomes`);
                 const data = await response.json();
-                console.log("Fetched data:", data);
                 const incomes = data.incomes;
 
                 let totalIncome = 0;
 
-                incomes.forEach(income => {
+                incomes.forEach((income) => {
                     if (income.user_id === userID) {
                         totalIncome += parseFloat(income.amount);
                     }
                 });
 
                 setIncome(totalIncome);
-                
             } catch (error) {
                 console.log("Error fetching income:", error);
-                
             }
         };
         fetchData();
@@ -60,22 +54,19 @@ function Home() {
             try {
                 const response = await fetch(`${API_URL}expenses`);
                 const data = await response.json();
-                console.log("Fetched data:", data);
                 const expenses = data.expenses;
 
                 let totalExpenses = 0;
 
-                expenses.forEach(expense => {
+                expenses.forEach((expense) => {
                     if (expense.user_id === userID) {
                         totalExpenses += parseFloat(expense.amount);
                     }
                 });
 
                 setExpense(totalExpenses);
-                
             } catch (error) {
                 console.log("Error fetching expenses:", error);
-            
             }
         };
         fetchData();
@@ -86,12 +77,11 @@ function Home() {
             try {
                 const response = await fetch(`${API_URL}debts`);
                 const data = await response.json();
-                console.log("Fetched data:", data);
                 const debts = data.debts;
 
                 let totalDebt = 0;
 
-                debts.forEach(debt => {
+                debts.forEach((debt) => {
                     if (debt.user_id === userID) {
                         totalDebt += parseFloat(debt.remaining_balance);
                     }
@@ -110,12 +100,11 @@ function Home() {
             try {
                 const response = await fetch(`${API_URL}assets`);
                 const data = await response.json();
-                console.log("Fetched data:", data);
                 const assets = data.assets;
 
                 let totalAssets = 0;
 
-                assets.forEach(asset => {
+                assets.forEach((asset) => {
                     if (asset.user_id === userID) {
                         totalAssets += parseFloat(asset.value);
                     }
@@ -142,35 +131,51 @@ function Home() {
     let tempDebt;
     if (balance < 0) {
         tempDebt = Math.abs(balance);
-    }else{
+    } else {
         tempDebt = 0;
     }
-    if(isLoading){
+    if (isLoading) {
         return (
             <div className="flex items-center justify-center h-screen">
-              <div className="text-center">
-                <svg className="animate-spin h-8 w-8 text-blue-600 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-                <p className="mt-2 text-gray-600">Loading user data...</p>
-              </div>
+                <div className="text-center">
+                    <svg
+                        className="animate-spin h-8 w-8 text-blue-600 mx-auto"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                        ></circle>
+                        <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
+                    <p className="mt-2 text-gray-600">Loading user data...</p>
+                </div>
             </div>
-          );
-        }
+        );
+    }
     return (
-        <div className="rounded-b-2xl flex flex-col gap-4 bg-gray-900 p-1 ">
+        <div className="flex flex-col bg-gray-900 h-screen w-screen">
             <Header onCurrencyChange={handleCurrencyChange} onLogout={() => console.log("Logged out")} />
-            <div>
+            <div className="flex justify-center">
                 <PieChart totalIncome={income} totalExpense={expense} />
             </div>
-            <div>
-                <h1 className="text-5xl font-bold text-white">
+            <div className="text-center">
+                <h1 className="text-3xl md:text-5xl font-bold text-white">
                     {currencySymbol} {formatNumber(balance)}
                 </h1>
                 <p className="text-white">left to spend</p>
             </div>
-            <div>
+            <div className="gap-4 mx-[5%] my-auto">
                 <Link to="/income">
                     <InfoCard title="Income" value={`${currencySymbol} ${formatNumber(income)}`} />
                 </Link>
@@ -184,7 +189,7 @@ function Home() {
                     <InfoCard title="Assets" value={`${currencySymbol} ${formatNumber(assets)}`} />
                 </Link>
             </div>
-            <Navbar/>
+            <Navbar />
         </div>
     );
 }
