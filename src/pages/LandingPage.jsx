40ce2d247002bd12ee90091eb5290f2a44379f48
Bjoseph23 <typicalomondi@gmail.com>
2024-08-14 03:57:58 +0300

    Added smooth animation to landimg page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,6 +4,8 @@ import mockupImage from '../assets/mockup.png';
 function LandingPage() {
     const API_URL = "https://barnes.onrender.com/";
     const [totalUsers, setTotalUsers] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -11,19 +13,49 @@ function LandingPage() {
                 const response = await fetch(`${API_URL}users`);
                 const data = await response.json();
                 const users = data.users;
-                const roundedUsers = Math.round(users.length / 10) * 10;
+                const roundedUsers = Math.round(users.length * 10);
                 setTotalUsers(roundedUsers);
+                setIsLoading(false);
+
             } catch (error) {
                 console.error("Error fetching user data:", error);
             }
         };
         fetchData();
     }, []);
-
+    if (isLoading) {
+        return (
+            <div className="bg-[#242424] min-h-screen text-7xl flex flex-col items-center justify-center p-10">
+                <div className="text-center ">
+                    <svg
+                        className="animate-spin h-8 w-8 text-blue-600 mx-auto"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                        ></circle>
+                        <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
+                    <p className="mt-2 text-gray-600">Loading user data...</p>
+                </div>
+            </div>
+        );
+    }
     return (
         <div className="bg-[#242424] min-h-screen flex flex-col items-center justify-center p-10">
             <h1 className="text-5xl font-bold mb-6 text-center text-white">Barnes</h1>
-            <h3 className="text-3xl mb-6 text-center text-white">
+            <h3 className="text-3xl mb-6 text-center text-white order-first font-semibold tracking-tight sm:text-5xl">
                 The Financial Manager to <span
                     className="animate-counter"
                     style={{ '--num-start': 0, '--num-end': totalUsers }}
@@ -62,11 +94,11 @@ function LandingPage() {
                         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
                             <dt className="text-base leading-7 text-gray-600">Assets under holding</dt>
                             <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                                $<span
+                                $ <span
                                     className="animate-counter"
                                     style={{ '--num-start': 0, '--num-end': 119 }}
                                 >
-                                    <span className="flex tabular-nums text-slate-900 text-5xl font-extrabold mb-2"></span> trillion
+                                    <span className="flex tabular-nums text-slate-900 text-5xl font-extrabold mb-2"></span> Million
                                 </span>
                             </dd>
                         </div>
